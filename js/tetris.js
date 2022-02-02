c3021d04b9a8a83e643c1785bc75279b6406a36c
Phil <znzpzp13@naver.com>
2022-02-02 23:47:07 +0900

    tetris clone 중 계속~ing

--- js/tetris.js
+++ js/tetris.js
@@ -57,7 +57,7 @@ function prependNewLine() {
     li.prepend(ul)
     playground.prepend(li)
 }
-function renderBlocks() { // 블럭 그림
+function renderBlocks(moveType= "") { // 블럭 그림
     const { type, direction, top, left} = tempMovingItem // 각각의 프로퍼티들을 변수로 바로 사용할 수 있도록 해줌
     const movingBlocks = document.querySelectorAll(".moving")
     movingBlocks.forEach(moving => {
@@ -65,7 +65,7 @@ function renderBlocks() { // 블럭 그림
     })
     // tempMovingItem.type 위처럼 안해주면 이런식으로 각각으로 접근해야하는 번거로움 있음
     // tempMovingItem.direction
-    BLOCKS[type][direction].forEach(block => {
+    BLOCKS[type][direction].some(block => { // forEach는 중간에 멈출수가 없어서 대신에 some 을 사용할 것임
         const x = block[0] + left// ul안의 li의 값
         const y = block[1] + top// li의 low값
         //console.log({playground}) // 출력 NodeList있음
@@ -84,6 +84,7 @@ function renderBlocks() { // 블럭 그림
                 }
             },0)// task queue로 잠깐 빼놓음
             // renderBlocks()// 재귀함수로 호출 그러나 콜스택 맥시멈, 액시드 같은 경우가 발생할 수 있으므로 settimeout으로 잠깐 처리
+            return true; // forEach는 이게 안되지만 some 은 가능
         }
 
     }) // 좌표를 가진 애들을 가져와서 forEach 적용
@@ -92,19 +93,42 @@ function renderBlocks() { // 블럭 그림
     movingItem.direction = direction
 }
 function seizeBlock() { // 더이상 내려갈 곳이 없을 때 블럭에서 moving 을 떼버리고 새로운 블럭을 만들어 줄 것임
-    console.log('seizeBlock')
+    const movingBlocks = document.querySelectorAll(".moving")
+    movingBlocks.forEach(moving => {
+        moving.classList.remove("moving")
+        moving.classList.add("seized") // 이 요소를 가졌다면~
+    })
+    generateNewBlock()
 }
+function generateNewBlock() { // seized끝나면 새로운 블럭이 생기게 됨
+    movingItem.type = 0
+    movingItem.left = 3 // 가운데 와야하므로 3
+    movingItem.direction = 0
+    tempMovingItem = {...movingItem}
+    renderBlocks()
+}
+
+// classList와 같이 쓰이는 함수 contains 그 클래스가 이것을 가지고 있는지 없는지 확인
 function checkEmpty(target) { // ture or false 를 담아줌
-    if(!target) {
-        return false;
+    if(!target || target.classList.contains("seized")) {
+        return false; //빈값이 아니다
     }
     return true;
 }
 
 function moveBlock(moveType, amount) {
     tempMovingItem[moveType] += amount// left를 바꿔주게 됨
-    renderBlocks()
+    renderBlocks(moveType)
 
+}
+function changeDirection() {
+    const direction = tempMovingItem.direction
+    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1
+    // tempMovingItem.direction += 1
+    // if (tempMovingItem.direction === 4) {
+    //     tempMovingItem.direction = 0
+    // } // 변수들이 반복되니까 가독성이 떨어지므로 삼항연산자에 담을 것임
+    renderBlocks()
 }
 
 //event handling
@@ -118,6 +142,9 @@ document.addEventListener("keydown", e => { // keycode 추출
             break;
         case 40:
             moveBlock("top", 1);
+            break;
+        case 38:
+            changeDirection()
             break;
         default :
             break;
